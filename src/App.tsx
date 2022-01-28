d011621864dc0cb053b59970acc763e0c74f0801
yamada-jikennya <92251718+yamada-jikennya@users.noreply.github.com>
2022-01-28 14:16:29 +0900

    Sprite Adding made

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
-import React from 'react';
-import Game from './modules/Game';
-import useWindowSize from './modules/useWindowSize';
+import React, { useState } from "react";
+import Game from "./modules/Game";
+import Sprite from "./modules/Sprite";
+import useWindowSize from "./modules/useWindowSize";
 
 const App: React.VFC = () => {
   const { width, height } = useWindowSize();
+  const [objs, setObjs] = useState<Sprite[]>([]);
+  let add: Sprite[];
+  let addFlag: boolian = false;
+  //画像のスプライト生成
+  let school: Sprite = new Sprite("./img/school_A5.png");
+  if (!objs.find(school)) {
+    add = [school];
+    addFlag = true;
+  }
+
+  const handleAdd = (obj: Sprite) => {
+    const ar = objs.concat([obj]);
+    setObjs(ar);
+  };
+
   return (
     <div>
-      <Game height={height * 0.95} width={width * 0.95} />
+      <Game
+        height={height * 0.95}
+        width={width * 0.95}
+        objs={objs}
+        onAdd={addFlag ? () => handleAdd(add) : null}
+      />
     </div>
   );
 };
--- src/modules/Game.tsx
+++ src/modules/Game.tsx
@@ -1,31 +1,51 @@
-import React, { useState, useEffect, useCallback, useRef } from 'react';
+import React, { useState, useEffect, useCallback, useRef } from "react";
+import Sprite from "./Sprite";
 
 type Props = {
   width: number;
+  //canvasの幅
   height: number;
+  //canvasの高さ
+  objs: Sprite[];
+  //ゲーム内オブジェクト一覧
+  onAdd: VoidFunction | null;
+  //追加する関数
 };
 
 const Game: React.VFC<Props> = (props) => {
+  /*状態の初期化・Props受け取り*/
   const [ctx, setCtx] = useState<CanvasRenderingContext2D | null>(null);
   const reqIdRef = useRef(0);
-  const { width, height } = props;
+  const { width, height, objs, onAdd } = props;
 
+  /*メソッド定義*/
   const mainloop = useCallback(() => {
     if (ctx !== null) {
-      ctx.fillStyle = '#000000';
+      ctx.fillStyle = "#000000";
       ctx.fillRect(0, 0, width, height);
+
+      if (onAdd !== null) {
+        onAdd();
+      }
+
+      for (let i = 0; i <= objs.length; i++) {
+        objs[i].update(ctx);
+      }
+
       reqIdRef.current = requestAnimationFrame(mainloop);
     }
   }, [ctx, height, width]);
 
+  /*canvas要素を取得・初期化*/
   useEffect(() => {
-    const canvas = document.getElementById('canvas') as HTMLCanvasElement;
+    const canvas = document.getElementById("canvas") as HTMLCanvasElement;
     canvas.width = width;
     canvas.height = height;
-    const context: CanvasRenderingContext2D | null = canvas.getContext('2d');
+    const context: CanvasRenderingContext2D | null = canvas.getContext("2d");
     setCtx(context);
   }, [width, height]);
 
+  /*状態にコンテキストが登録されたらmainloop開始 */
   useEffect(() => {
     if (ctx !== null) {
       mainloop();
