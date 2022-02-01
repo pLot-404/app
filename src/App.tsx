c560a4f71a3f94eb632632a1ef66cf55d3e308c1
Silvia-moon <inouibuk326@sgh-tsukuba.org>
2022-02-01 21:20:56 +0900

    Updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,55 @@
-import React, { useState, useEffect, useRef } from "react";
-import Sprite from "./modules/Sprite";
-import useWindowSize from "./modules/useWindowSize";
+import React, { useState, useEffect, useRef } from 'react';
+import Sprite from './modules/Sprite';
+import useWindowSize from './modules/useWindowSize';
 
-addEventListener("load", () => {
-  const App: React.VFC = () => {
-    const { width: rawWidth, height: rawHeight } = useWindowSize();
-    const width = rawWidth * 0.95;
-    const height = rawHeight * 0.95;
-    const [objs, setObjs] = useState<Sprite[]>([]);
-    const [ctx, setCtx] = useState<CanvasRenderingContext2D | null>(null);
-    const reqIdRef = useRef(0);
-    let objes: Sprite[] = objs;
+const App: React.VFC = () => {
+  const { width: rawWidth, height: rawHeight } = useWindowSize();
+  const width = rawWidth * 0.95;
+  const height = rawHeight * 0.95;
+  const [objs, setObjs] = useState<Sprite[]>([]);
+  const [ctx, setCtx] = useState<CanvasRenderingContext2D | null>(null);
+  const reqIdRef = useRef(0);
+  let objes: Sprite[] = objs;
 
-    /* メソッド定義 */
-    const mainloop = () => {
-      if (ctx !== null) {
-        ctx.fillStyle = "#000000";
-        ctx.fillRect(0, 0, width, height);
+  /* メソッド定義 */
+  const mainloop = () => {
+    if (ctx !== null) {
+      ctx.fillStyle = '#000000';
+      ctx.fillRect(0, 0, width, height);
 
-        for (let i = 0; i <= objes.length - 1; i++) {
-          objes[i].update(ctx);
-        }
-        reqIdRef.current = requestAnimationFrame(mainloop);
+      for (let i = 0; i <= objes.length - 1; i++) {
+        objes[i].update(ctx);
       }
-    };
-
-    const add = (obj: Sprite) => {
-      setObjs(objs.concat([obj]));
-      objes = objs.concat([obj]);
-    };
-
-    /* canvas要素を取得・初期化 */
-    useEffect(() => {
-      const canvas = document.getElementById("canvas") as HTMLCanvasElement;
-      canvas.width = width;
-      canvas.height = height;
-      const context: CanvasRenderingContext2D | null = canvas.getContext("2d");
-      setCtx(context);
-    }, [width, height]);
-
-    /* 状態にコンテキストが登録されたらmainloop開始 */
-    useEffect(() => {
-      if (ctx !== null) {
-        const school: Sprite = new Sprite("./img/school_A5.png");
-        add(school);
-        mainloop();
-      }
-      return () => cancelAnimationFrame(reqIdRef.current);
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [ctx]);
-
-    return <canvas id="canvas" />;
+      reqIdRef.current = requestAnimationFrame(mainloop);
+    }
   };
 
-  export default App;
-});
+  const add = (obj: Sprite) => {
+    setObjs(objs.concat([obj]));
+    objes = objs.concat([obj]);
+  };
+
+  /* canvas要素を取得・初期化 */
+  useEffect(() => {
+    const canvas = document.getElementById('canvas') as HTMLCanvasElement;
+    canvas.width = width;
+    canvas.height = height;
+    const context: CanvasRenderingContext2D | null = canvas.getContext('2d');
+    setCtx(context);
+  }, [width, height]);
+
+  /* 状態にコンテキストが登録されたらmainloop開始 */
+  useEffect(() => {
+    if (ctx !== null) {
+      const school: Sprite = new Sprite('./img/school_A5.png');
+      add(school);
+      mainloop();
+    }
+    return () => cancelAnimationFrame(reqIdRef.current);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [ctx]);
+
+  return <canvas id="canvas" />;
+};
+
+export default App;
