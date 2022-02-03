c444c1ca4cb51706cb397187c101eecabe91e747
yamada-jikennya <92251718+yamada-jikennya@users.noreply.github.com>
2022-02-03 16:56:22 +0900

    a

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,104 @@
+// const path = require("path");
+// const HtmlWebpackPlugin = require("html-webpack-plugin");
+//
+//
+
+// module.exports = {
+//   //モードをdevelopment、production、noneから設定（必須）
+//   //development: 開発時のファイル出力モード（最適化より時間短縮、エラー表示を優先）
+//   //production: 本番時のファイル出力モード（最適化されて出力）
+//   mode: "development",
+//   //メインとなるjsファイル(エントリーポイント)
+//   entry: "./src/index.ts",
+//   //ファイルの出力設定
+//   output: {
+//     path: path.join(__dirname, "/dist"), //出力先のディレクトリ（絶対パスで指定）
+//     filename: "js/bundle.js", //出力ファイル名
+//   },
+//   //デバッグのためのSourceMap（ビルド前後の対応関係を記述したファイル）の出力設定
+//   devtool: "inline-source-map",
+//   //対象のファイルを変換するためのloaderを設定
+//   module: {
+//     rules: [
+//       {
+//         test: /\.scss$/i,
+//         use: [
+//           {
+//             loader: MiniCssExtractPlugin.loader,
+//           },
+//           {
+//             loader: "css-loader",
+//           },
+//           {
+//             loader: "sass-loader",
+//             options: {
+//               sassOptions: {
+//                 outputStyle: "expanded",
+//               },
+//             },
+//           },
+//         ],
+//       },
+//       {
+//         test: /\.ts?$/, //build対象（loaderを適用するファイル）を指定
+//         loader: "ts-loader", //適用するloaderを指定
+//       },
+//     ],
+//   },
+//   //importの際に省略する対象の拡張子を配列で指定
+//   resolve: {
+//     extensions: [".ts", ".js", ".json"], //指定されている拡張子のファイルはimportの際に拡張子を省略できる
+//   },
+//   //webpack-dev-serverの設定
+//   devServer: {
+//     static: path.join(__dirname, "/dist"), //表示する静的ファイル（HTML）の場所を指定
+//     open: true, //ブラウザを自動的に起動
+//     port: 3000, //ポート番号を指定（デフォルトのポート：8080）
+//   },
+//   //pluginの設定
+//   plugins: [
+//     new HtmlWebpackPlugin({
+//       //webpackでbuildされたJSやCSSを表示するHTMLを自動的に生成するplugin
+//       template: "./public/index.html", //テンプレートとして使用するHTMLファイルを指定
+//       filename: "index.html", //生成するHTMLファイル名
+//     }),
+//     new CopyWebpackPlugin({
+//       patterns: [{ from: "./public/static", to: "./" }],
+//     }),
+//     new MiniCssExtractPlugin({
+//       filename: "css/style.css",
+//       ignoreOrder: true,
+//     }),
+//   ],
+// };
+
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-
 
 module.exports = {
-  //モードをdevelopment、production、noneから設定（必須）
-  //development: 開発時のファイル出力モード（最適化より時間短縮、エラー表示を優先）
-  //production: 本番時のファイル出力モード（最適化されて出力）
   mode: "development",
-  //メインとなるjsファイル(エントリーポイント)
-  entry: "./src/index.ts",
-  //ファイルの出力設定
+  // mode: "production", //Delete slash when you release
+  entry: path.join(__dirname, "src", "App.ts"),
+
   output: {
-    path: path.join(__dirname, "/dist"), //出力先のディレクトリ（絶対パスで指定）
-    filename: "js/bundle.js", //出力ファイル名
+    path: path.join(__dirname, "build"),
+    filename: "js/bundle.js",
   },
-  //デバッグのためのSourceMap（ビルド前後の対応関係を記述したファイル）の出力設定
+
   devtool: "inline-source-map",
-  //対象のファイルを変換するためのloaderを設定
+
+  target: "es5",
+
   module: {
     rules: [
       {
         test: /\.scss$/i,
         use: [
           {
-						loader: MiniCssExtractPlugin.loader,
-					},
+            loader: MiniCssExtractPlugin.loader,
+          },
           {
             loader: "css-loader",
           },
@@ -41,34 +113,37 @@ module.exports = {
         ],
       },
       {
-        test: /\.ts?$/, //build対象（loaderを適用するファイル）を指定
-        loader: "ts-loader", //適用するloaderを指定
+        test: /\.ts?$/,
+        loader: "ts-loader",
       },
     ],
-  },
-  //importの際に省略する対象の拡張子を配列で指定
+  }, //TO DO
+
   resolve: {
-    extensions: [".ts", ".js", ".json"], //指定されている拡張子のファイルはimportの際に拡張子を省略できる
+    extensions: [".js", ".ts", ".scss", ".json"],
   },
-  //webpack-dev-serverの設定
+
   devServer: {
-    static: path.join(__dirname, "/dist"), //表示する静的ファイル（HTML）の場所を指定
-    open: true, //ブラウザを自動的に起動
-    port: 3000, //ポート番号を指定（デフォルトのポート：8080）
+    contentBase: path.join(__dirname, "build"),
+    open: true,
+    overlay: {
+      warnings: true,
+      errors: true,
+    },
+    port: 3000,
   },
-  //pluginの設定
+
   plugins: [
     new HtmlWebpackPlugin({
-      //webpackでbuildされたJSやCSSを表示するHTMLを自動的に生成するplugin
-      template: "./public/index.html", //テンプレートとして使用するHTMLファイルを指定
-      filename: "index.html", //生成するHTMLファイル名
+      template: "./public/index.html",
+      filename: "index.html",
     }),
     new CopyWebpackPlugin({
       patterns: [{ from: "./public/static", to: "./" }],
     }),
     new MiniCssExtractPlugin({
-			filename: 'css/style.css',
-			ignoreOrder: true,
-		})
+      filename: "css/style.css",
+      ignoreOrder: true,
+    }),
   ],
 };
