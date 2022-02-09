6de96dcc8809c7e4198bb688841ecee84d358f72
yamada-jikennya <92251718+yamada-jikennya@users.noreply.github.com>
2022-02-09 18:21:05 +0900

    add Tile

--- src/modules/Tile.ts
+++ src/modules/Tile.ts
@@ -1,3 +1,7 @@
 import Sprite from "./Sprite";
 
-export default class Tile extends Sprite {}
+export default class Tile extends Sprite {
+  constructor(img, public size: number = 40) {
+    super(img,size,size);
+  }
+}
